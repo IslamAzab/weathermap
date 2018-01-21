69a5c8e2065ca80eec862a7a5d8a87bbbfbac00c
Islam Azab <islam.3azab@gmail.com>
2018-01-22 00:55:04 +0100

    refactor visitors controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,24 +2,24 @@ require 'open_weather'
 class VisitorsController < ApplicationController
   def index
     city = SAMPLE_CITIES.sample
-    weather = OpenWeather::Current.city(city, OPEN_WEATHER_OPTIONS)
-    if weather["cod"]
-      @response = "#{city} is not found!"
-    else
-      @response = Nokogiri::HTML(weather).css('body')
-    end
+    weather_data city
   end
 
   def city
     city = params[:city_name]
     p params
     p city
+    weather_data city
+    render :index
+  end
+
+  private
+  def weather_data(city)
     weather = OpenWeather::Current.city(city, OPEN_WEATHER_OPTIONS)
     if weather["cod"]
       @response = "#{city} is not found!"
     else
       @response = Nokogiri::HTML(weather).css('body')
     end
-    render :index
   end
 end
