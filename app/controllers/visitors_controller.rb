c3c605fbd8096ed0b0cfe4ec78388e12e72b3271
Islam Azab <islam.3azab@gmail.com>
2018-01-22 00:22:43 +0100

    update index controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,12 @@
+require 'open_weather'
 class VisitorsController < ApplicationController
+  def index
+    city = SAMPLE_CITIES.sample
+    weather = OpenWeather::Current.city(city, OPEN_WEATHER_OPTIONS)
+    if weather["cod"]
+      @response = "#{city} is not found!"
+    else
+      @response = Nokogiri::HTML(weather).css('body')
+    end
+  end
 end
