abe4ae532af040e8badb312b0a1e737ec4b7867f
Islam Azab <islam.3azab@gmail.com>
2018-01-22 01:06:43 +0100

    add test for vaild city search

--- spec/features/visitors/index_page_spec.rb
+++ spec/features/visitors/index_page_spec.rb
@@ -7,7 +7,7 @@ feature 'Index page' do
   # Scenario: Visit the index page
   #   Given I am a visitor
   #   When I visit the index page
-  #   Then I see "Welcome"
+  #   Then I see weather data
   scenario 'visit the index page' do
     visit root_path
     expect(page).to have_content 'Clouds'
@@ -16,4 +16,20 @@ feature 'Index page' do
     expect(page).to have_content 'Pressure'
   end
 
+  # Scenario: Visit the index page
+  #   Given I am a visitor
+  #   When I search the index page for a valid city
+  #   Then I see weather data for my city
+  scenario 'search in the index page for a valid city' do
+    visit '/'
+
+    fill_in :city_name, with: 'Berlin'
+    click_button :submit
+
+    expect(page).to have_content 'Berlin'
+    expect(page).to have_content 'Clouds'
+    expect(page).to have_content 'Humidity'
+    expect(page).to have_content 'Wind'
+    expect(page).to have_content 'Pressure'
+  end
 end
